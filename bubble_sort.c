cc3a8fbc1e0d226c80cf5a7760df8175ff857af5
TomasNagy <tomi.nagy@outlook.com>
2023-05-29 14:19:55 +0200

    imp: bubble sort algorithm
    wip: bubble sort algorithm
    
    imp: bubble sort algorithm

--- bubble_sort.c
+++ bubble_sort.c
@@ -5,4 +5,32 @@
 int main(void)
 {
     int unsorted_array[ARRAY_LENGTH] = {0, 4, 3, 10, 6, 2, 8, 7, 5, 9, 1};
+
+    printf ("Unsorted: ");
+
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        printf ("%i ", unsorted_array[i]);
+    }
+    printf ("\n");
+
+
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        for (int j = 0; j < ARRAY_LENGTH-1; j++)
+        {
+            if (unsorted_array[j] > unsorted_array[j + 1])
+            {
+                int temp = unsorted_array[j];
+                unsorted_array[j] = unsorted_array[j + 1];
+                unsorted_array[j + 1] = temp;
+            }
+        }
+    }
+
+    printf ("Sorted: ");
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        printf ("%i ", unsorted_array[i]);
+    }
 }
